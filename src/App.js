ff906457b99f526b6cb9e499ebf16c978f1b4d4d
swapshivam3 <72791604+swapshivam3@users.noreply.github.com>
2021-06-04 00:07:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Images from './components/Images/Images';
 import Road from './components/Road/Road';
 import image_css from './components/Images/Images.css';
-const path = './components/Images/Untitled.jpg';
+//const path = './components/Images/Untitled.jpg';
 
 function App() {
   return (
