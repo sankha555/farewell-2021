55cf081bd3cda88c100f8e5b6b6d3e2906c554f0
swapshivam3 <swapshivam3@gmail.com>
2021-06-06 23:48:18 +0530

    Complete Rotate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import Images from './components/Images/Images';
 import Rotate from './components/Rotate/Rotate';
-import RotateBackground from './components/RotateBackground/RotateBackground'
-import RotateBackground_css from './components/RotateBackground/RotateBackground.css'
-
 import Road from './components/Road/Road';
 import image_css from './components/Images/Images.css';
 import img_path from './components/Images/Untitled.jpg';
@@ -13,8 +10,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-        {/* <Images path={img_path} css={image_css}/>    */}
-                <RotateBackground/>   
+               
 
         <Rotate/>
       </div>
--- src/components/Rotate/Rotate.js
+++ src/components/Rotate/Rotate.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {useEffect, useState} from 'react';
 import {Dimensions} from 'react-native';
 import './Rotate.css';
+import Particles from 'react-particles-js';
+
 
 const window = Dimensions.get("window");    
 
@@ -17,15 +19,53 @@ const window = Dimensions.get("window");
     return () => {
       Dimensions.removeEventListener("change", onChange);
     };
-  });
+  });     
+
           if (dimensions.window.width<dimensions.window.height) {
-             return (
-        <div className="phone-container">
+             return (<div>
+               <div class='bg'>
+                 <div className="phone-container" >
         <div className="phone">
           </div>
           <div className="message">
             Please rotate your device!
         </div>
+        </div>
+<Particles
+    width={dimensions.window.width} height={dimensions.window.height} params={{
+	    "particles": {
+	        "number": {
+	            "value": 100
+	        },
+	        "size": {
+	            "value": 3
+	        },
+            "color": {
+            "value":"#ffffff"
+        },
+         "opacity": {
+              "value": 0.1,
+            },
+            "links": {
+              "opacity": 0.3,
+            },
+          },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        }
+	    },
+      "background": {
+            "color": {
+              "value": "#000000",
+            },
+          }
+	}} />
+</div>
+
         </div>
         );
       }
