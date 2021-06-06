7442c4fc702dc7dce43927bbd2d36c39c8b51103
harshneema <74879445+harshneema@users.noreply.github.com>
2021-06-06 11:27:33 +0530

    Create Spinner.js

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../Spinner/Spinner.css'
+
+function Spinner(props) {
+    return ( <div>
+    <div id="wrapD3Cube">
+        <div id="D3Cube">
+            <div id="side1"><div id = "f1"></div></div>
+            <div id="side2"><div id = "f2"></div></div>
+            <div id="side3"><div id = "f3"></div></div>
+            <div id="side4"><div id = "f4"></div></div>
+            <div id="side5"><div id = "f5"></div></div>
+            <div id="side6"></div>
+        </div>
+    </div></div> );
+}
+
+
+export default Spinner
