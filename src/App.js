d43d036f8744513b809d1017721da2659b9e3faa
swapshivam3 <swapshivam3@gmail.com>
2021-06-07 00:16:10 +0530

    add modal

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-               
-
         <Rotate/>
       </div>
   );
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react'
+import { Button } from 'react-bootstrap'
+import './content.css'
+
+function Modal() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Load Content
+        </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <>
+            <div id="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eum molestias aliquid ipsa, rem soluta. Sapiente
+              quasi ea voluptatibus officia dolorem nobis obcaecati, quam aliquid
+    </div>
+
+            <div id="img">
+              {/* <img src="" /> */}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt cumque accusamus delectus fugiat reiciendis! Modi corrupti ducimus nisi voluptatibus? Nihil ipsam nam aliquid vero! Animi adipisci maiores
+    </div>
+          </>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+            </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+            </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+
+export default Modal
