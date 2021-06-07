08834a6d92d75b1a1d90560171618957f5a36486
Nachiket <nachikotalwar@gmail.com>
2021-06-07 22:30:03 +0530

    fixed app

--- src/components/Doodle/Doodle.js
+++ src/components/Doodle/Doodle.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Doodle.css';
 
-const Doodle =  () => {
+const Doodle =  (props) => {
     return(
-        <div className="doodle">
+        <div className="doodle" style={{transform: `translate(${props.move}px, 0px)`}}>
             
         </div>
     )
--- src/components/Road/Road.js
+++ src/components/Road/Road.js
@@ -1,25 +1,64 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './Road.css';
 import Doodle from '../Doodle/Doodle';
+import Milestone from '../Milestone/Milestone';
+
 
 function Road() {
-  const onWheel = e => {
-    e.preventDefault();
-    const container = scrollRef.current;
-    const containerScrollPosition = scrollRef.current.scrollLeft;
+    
 
-    container.scrollTo({
-      top: 0,
-      left: containerScrollPosition + e.deltaY
-    });
-  };
+    const [position, setPosition] = useState(0);
 
-  const scrollRef = useRef(null);
+    const onWheel = (e) => {
+        e.preventDefault();
+        const container = scrollRef.current;
+        const containerScrollPosition = scrollRef.current.scrollLeft;
+
+        container.scrollTo({
+            top: 0,
+            left: containerScrollPosition + e.deltaY
+        });
+
+        if ((position + e.deltaY*1.1 > 15) && (position+e.deltaY*1.1 < 5900)) {
+            setPosition(position + e.deltaY*1.1);
+        }
+        
+    };
+
+    function useEvent(event, handler){
+        useEffect(() => {
+            window.addEventListener(event, handler);
+            return function cleanup() {
+                window.removeEventListener(event, handler);
+            }
+        } )
+    }
+    
+    const moveRight = (e) => {
+        e.preventDefault();
+        if (e.key === ' ') {
+            setPosition(position + 50);
+        }
+        // const doodle = document.getElementById("moving_doodle");
+        // if (doodle) {
+        //     const doodlePosition = doodle.style.right;
+        //     doodle.style.right = doodlePosition + 50 + 'px';
+        // }
+    }
+
+    useEvent('keyup', moveRight);
+
+    
+
+    const scrollRef = useRef(null);
+
     
     let dots = '.';
     for (let i= 0; i < 200; i++) {
         dots = dots.concat('*.*.*.');
     }
+
+    const milestones = ['First Year', 'Second Year', 'Third Year', 'Fourth Year'];
 
   return (
       <React.Fragment>
@@ -27,12 +66,12 @@ function Road() {
         ref={scrollRef}
         className="road"
         onWheel={onWheel}
-      >
-            <Doodle />
+      > 
+              <Doodle id="moving_doodle" move={position}/>
               <p style={{ color: "white" }}>
                 {dots}
-            </p>
-        
+              </p>
+              {milestones.map( (milestone, index) => <Milestone left={1250*(index+1)} milestone={milestone} />)}
           </div>
           </React.Fragment>
     
