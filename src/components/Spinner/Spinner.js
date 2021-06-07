3d646b140a538f0b4a89cf9ba1421cda20781d19
harshneema <74879445+harshneema@users.noreply.github.com>
2021-06-07 21:54:49 +0530

    Update Spinner.js

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import '../Spinner/Spinner.css'
 
+
 function Spinner(props) {
-    return ( <div>
+    return ( <div id="spinnerdiv">
     <div id="wrapD3Cube">
-        <div id="D3Cube">
-            <div id="side1"><div id = "f1"></div></div>
-            <div id="side2"><div id = "f2"></div></div>
-            <div id="side3"><div id = "f3"></div></div>
-            <div id="side4"><div id = "f4"></div></div>
-            <div id="side5"><div id = "f5"></div></div>
+        <div id="D3Cube"><div id="side1"><div id="main1"></div><div id="sidediv1"></div>
+            </div>
+            <div id="side2"><div id="main2"></div><div id="sidediv2"></div></div>
+            <div id="side3"><div id="main3"></div><div id="sidediv3"></div></div>
+            <div id="side4"><div id="main4"></div><div id="sidediv4"></div></div>
+            <div id="side5"><div id="main5"></div><div id="sidediv5"></div></div>
             <div id="side6"></div>
         </div>
-    </div></div> );
+     </div>
+    <p id= "spinner_txt">CODING CLUB</p>
+    <div id="loader"></div>
+    </div> );
 }
 
 
