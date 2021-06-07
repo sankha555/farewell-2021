a263131520b6a27d6960a995cd93fccb42ec0a13
Nachiket <nachikotalwar@gmail.com>
2021-06-07 22:36:18 +0530

    fixed app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Road from './components/Road/Road';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-
+        Learn React
         </a>
       </header>
     </div>
